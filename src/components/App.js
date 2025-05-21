8b2c3474528871aeaeae4e43629dc1803e5249e2
Nayan <nayanj8871@gmail.com>
2025-05-22 01:36:16 +0530

    Header component its css added and updated index.css

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 function App() {  
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
 
       
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import headerStyles from "../styles/header.module.css"
+
+const Header = () =>{
+    return(
+        <div className={headerStyles.  }>
+
+        </div>
+    )
+}
+export default Header;
